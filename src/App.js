e78f22e9690841ea14ecdde46ef81ff8c0b265e0
AJFree458 <rthr.frmn@gmail.com>
2022-02-02 15:24:59 -0500

    'AS' prop and update the styled library

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 // import styled from "styled-components";
+import { HipsterButton } from "./components/Buttons";
 // import ComplexTitle from "./components/ComplexTitle";
 // import AlternativeTitle from "./components/AlternativeTitle";
 // import Card from "./components/Card";
-import Loading from "./components/Loading";
+// import Loading from "./components/Loading";
 
 function App() {
   return (
     <div style={{ padding: "2rem" }}>
-      <Loading />
+      <HipsterButton>click me</HipsterButton>
+      <HipsterButton as="a" href="https://www.johnsmilga.com/">
+        click me
+      </HipsterButton>
+      <HipsterButton>click me</HipsterButton>
     </div>
   );
 }
