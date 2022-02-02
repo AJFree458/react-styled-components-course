1571b077fe64fd95a84467d249baf528d5c30e7f
AJFree458 <rthr.frmn@gmail.com>
2022-02-02 15:03:32 -0500

    Setup themes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-// import styled from "styled-components";
+import { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
 // import ComplexTitle from "./components/ComplexTitle";
 // import AlternativeTitle from "./components/AlternativeTitle";
-import Card from "./components/Card";
+// import Card from "./components/Card";
+import GlobalStyles from "./global-style";
+
+const BaseTheme = {
+  color: "#222",
+  background: "#fff",
+};
+const DarkTheme = {
+  color: "#fff",
+  background: "#222",
+};
+
+const Container = styled.div`
+  padding: 2rem;
+  color: ${props => props.theme.color};
+  background: ${props => props.theme.background};
+`;
 
 function App() {
+  const [baseTheme, setTheme] = useState(true);
+
+  const toggleTheme = () => {
+    setTheme(!baseTheme);
+  };
   return (
-    <div style={{ padding: "2rem" }}>
+    <ThemeProvider theme={baseTheme ? BaseTheme : DarkTheme}>
+      <GlobalStyles />
+      <Container>
+        <h1>hello world</h1>
+        <button className="btn" onClick={toggleTheme}>
+          toggle me
+        </button>
+      </Container>
       {/* <ComplexTitle title={"more complex title"} />
       <AlternativeTitle title={"alternative title"} /> */}
-      <Card />
-    </div>
+      {/* <Card /> */}
+    </ThemeProvider>
   );
 }
 
